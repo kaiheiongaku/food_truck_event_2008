6d02ba9b6cab0fb0f81f40ef99f019c3a418b995
Robert Heath <robert.b.heathii@gmail.com>
2020-09-15 14:30:33 -0500

    Test and code check stock

---
+++ lib/food_truck.rb
@@ -0,0 +1,14 @@
+require './lib/item'
+
+class FoodTruck
+  attr_reader :name, :inventory
+
+  def initialize(name)
+    @name = name
+    @inventory = Hash.new(0)
+  end
+
+  def check_stock(item)
+    @inventory[:item]
+  end
+end
--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -1,5 +1,23 @@
 require './lib/item'
+require './lib/food_truck'
 require 'minitest/autorun'
 require 'minitest/pride'
 
-class ItemTest < Minitest::Test
+class FoodTruckTest < Minitest::Test
+
+  def test_it_exists_and_has_attributes
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+
+    assert_instance_of FoodTruck, food_truck
+    assert_equal "Rocky Mountain Pies", food_truck.name
+    assert_equal ({}), food_truck.inventory
+  end
+
+  def test_check_stock
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+
+    assert_equal 0, food_truck.check_stock(item1)
+  end
+end
